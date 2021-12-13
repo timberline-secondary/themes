84eb571104697e30f406820fbbb5f7bb9546ae2a
matt <matt@dont-ping.me>
2021-12-13 15:06:47 -0800

    feat: opt to use .env

--- __main__.py
+++ __main__.py
@@ -2,18 +2,21 @@ import os.path
 import pygame
 
 
+THEMEPATH = os.getenv('THEMEPATH')
+BYPASS = os.getenv('BYPASS')
+
 def find(name):
-    if os.path.exists(f"./songs/{name}.mp3"):
+    if os.path.exists(f"{THEMEPATH}{name}.mp3"):
         print("File exists.\n")
         pygame.mixer.init()
-        pygame.mixer.music.load(f"./songs/{name}.mp3")
+        pygame.mixer.music.load(f"{THEMEPATH}{name}.mp3")
         pygame.mixer.music.play()
         # !! Need to test how loud, if too loud use pygame.mixer.set_volume(float)
         while pygame.mixer.music.get_busy():
             continue
         return
     else:
-        print(f"No file found @ ./songs/{name}.mp3\n")
+        print(f"File '{name}.mp3' not found.\n")
         return
 
 
