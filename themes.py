548db86818040dcbaf680b7204cd33cffc70386e
tylerecouture <tylerecouture@gmail.com>
2021-12-14 11:31:29 -0800

    refactor to OOP with ThemePlayer class

--- themes.py
+++ themes.py
@@ -3,54 +3,66 @@ import pygame
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
-load_dotenv()
 
-THEME_PATH = os.getenv('THEME_PATH')
-SPAMBLOCK_PASSWORD = os.getenv('SPAMBLOCK_PASSWORD')
-DEFAULT_SPAMBLOCK = os.getenv('DEFAULT_SPAMBLOCK')
+class ThemePlayer():
 
-spamblock = {"time": DEFAULT_SPAMBLOCK, "waiting": False}
-latest = {}
+    def __init__(self, spamblock_time, theme_path, spamblock_pw):
+        self.theme_path = theme_path
+        self.spamblock_pw = spamblock_pw
+        self.spamblock_time = spamblock_time
+        self.waiting_for_spamblock_minutes_entry = False
+        self.latest = {}
 
+    def find(self, value):
 
-def find(name):
-    if spamblock["waiting"]:  # if the spamblock is waiting for minute entry
-        spamblock["time"] = name
-        print(f"Spam block is now set to {spamblock['time']}")
-        spamblock["waiting"] = False
-        return
-    if name == SPAMBLOCK_PASSWORD:  # if the code entered is the spamblock password
-        spamblock["waiting"] = True
-        return
-    if os.path.exists(f"{THEME_PATH}{name}.mp3"):  # if the file exists
-        print("File exists.\n")
-        if not spamblock["time"] and name in latest and latest[name] + timedelta(
-                minutes=int(spamblock["time"])) > datetime.now():
-            print(f"{name} is too recent.")
+        if self.waiting_for_spamblock_minutes_entry:
+            self.spamblock_time = value
+            print(f"Spam block is now set to {self.spamblock_time}")
+            waiting_for_spamblock_minutes_entry = False
             return
-        pygame.mixer.music.load(f"{THEME_PATH}{name}.mp3")
+
+        if value == self.spamblock_pw:  # if the code entered is the spamblock password
+            waiting_for_spamblock_minutes_entry = True
+            return
+            
+        if os.path.exists(f"{self.theme_path}{value}.mp3"):  # if the file exists
+            print("File exists.\n")
+            if not self.spamblock_time and value in self.latest and self.latest[value] + timedelta(
+                    minutes=int(self.spamblock_time)) > datetime.now():
+                print(f"{value} is too recent.")
+                return
+            pygame.mixer.music.load(f"{self.theme_path}{value}.mp3")
+            pygame.mixer.music.play()
+            self.latest[value] = datetime.now()
+
+            # !! Need to test how loud, if too loud use pygame.mixer.set_volume(float)
+            while pygame.mixer.music.get_busy():
+                continue
+            return
+        else:
+            print(f"File '{value}.mp3' not found.\n")
+            return
+
+
+    def run(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load("./start.mp3")
         pygame.mixer.music.play()
-        latest[name] = datetime.now()
-        # !! Need to test how loud, if too loud use pygame.mixer.set_volume(float)
-        while pygame.mixer.music.get_busy():
-            continue
-        return
-    else:
-        print(f"File '{name}.mp3' not found.\n")
-        return
+        while True:
+            print("Please enter the code:")
 
+            code = input("> ")
 
-def main():
-    pygame.mixer.init()
-    pygame.mixer.music.load("./start.mp3")
-    pygame.mixer.music.play()
-    while True:
-        print("Please enter the code:")
-
-        code = input("> ")
-
-        find(code)
+            self.find(code)
 
 
 if __name__ == "__main__":
-    main()
+    
+    load_dotenv()
+
+    THEME_PATH = os.getenv('THEME_PATH')
+    SPAMBLOCK_PASSWORD = os.getenv('SPAMBLOCK_PASSWORD')
+    DEFAULT_SPAMBLOCK = os.getenv('DEFAULT_SPAMBLOCK')
+
+    player = ThemePlayer(DEFAULT_SPAMBLOCK, THEME_PATH, SPAMBLOCK_PASSWORD)
+    player.run()
