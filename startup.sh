1273ea7af5c51a62ea68839da7b732d27a9c6803
matt <matt@dont-ping.me>
2021-12-14 13:23:03 -0800

    fix(startup.sh): fix startup python command

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -dm bash -c 'git pull && pip install -r requirements.txt && python theme.py'
+screen -dm bash -c 'git pull && pip install -r requirements.txt && python3 themes.py'
