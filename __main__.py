9359abaa2509da01336625708c559514fe7168f2
matt <matt@dont-ping.me>
2021-12-13 16:28:39 -0800

    feat(__main__.py): add spamblock functionality

--- __main__.py
+++ __main__.py
@@ -1,19 +1,30 @@
 import os.path
 import pygame
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
 THEMEPATH = os.getenv('THEMEPATH')
 BYPASS = os.getenv('BYPASS')
 
+bypass = {"state": False}
+latest = {}
 
 def find(name):
+    if name == BYPASS:
+        bypass["state"] = not bypass["state"]
+        print(f"Spam block is now {'off' if bypass['state'] else 'on'}")
+        return
     if os.path.exists(f"{THEMEPATH}{name}.mp3"):
         print("File exists.\n")
+        if not bypass["state"] and name in latest and latest[name] + timedelta(minutes=2) > datetime.now():
+            print(f"{name} is too recent.")
+            return
         pygame.mixer.init()
         pygame.mixer.music.load(f"{THEMEPATH}{name}.mp3")
         pygame.mixer.music.play()
+        latest[name] = datetime.now()
         # !! Need to test how loud, if too loud use pygame.mixer.set_volume(float)
         while pygame.mixer.music.get_busy():
             continue
@@ -27,7 +38,7 @@ def main():
     while True:
         print("Please enter the code:")
 
-        code = input()
+        code = input("> ")
 
         find(code)
 
