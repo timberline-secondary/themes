a19b8936844bbbbbc53d0d74cad5eb124dad46e7
matt <matt@dont-ping.me>
2021-12-14 12:42:54 -0800

    refactor: remove redundant command from startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -dm bash -c 'cd /home/pi/themes && git pull && pip install -r requirements.txt && python theme.py'
+screen -dm bash -c 'git pull && pip install -r requirements.txt && python theme.py'
