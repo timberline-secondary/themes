0875b36ff6b2f5ac7d20613e38696eea4f73a3b0
matt <matt@dont-ping.me>
2021-12-15 12:44:02 -0800

    fix(startup): use pip3

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 export DISPLAY=:0
 
 git pull
-pip install -r requirements.txt
-lxterminal -e "cd /home/pi/themes && python3.9 themes.py"
+pip3 install -r requirements.txt
+lxterminal -e "cd /home/pi/themes && python themes.py"
