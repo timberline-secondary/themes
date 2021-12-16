8601ee734b91f490b73b89450d5bf928cdca5982
tylerecouture <tylerecouture@gmail.com>
2021-12-16 14:03:35 -0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo "Starting Soundboard.."
 
+echo "Turning on numlock"
 # Turn numlock on for the TTYs:
 for tty in /dev/tty[1-6]; do
     /usr/bin/setleds -D +num < "$tty";
 done
 
+echo "Checking for updates..."
 git pull
+echo "Updating dependings, sorry this is sloooooooooo"
 python -m pip install -r requirements.txt
+echo "Finally starting!  You should hear the startup sound."
 python themes.py
