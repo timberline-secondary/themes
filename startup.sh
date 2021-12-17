b5bde1b574c98d2f0f96ba5791cf2be8541c93e2
tylerecouture <tylerecouture@gmail.com>
2021-12-16 16:52:10 -0800

    only run pip install if there are changes to the requirements.txt file

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting Soundboard.."
+echo "Starting Entrance Theme Machine."
 
 echo "Turning on numlock"
 # Turn numlock on for the TTYs:
@@ -9,7 +9,14 @@ done
 
 echo "Checking for updates..."
 git pull
-echo "Updating dependings, sorry this is sloooooooooo"
-python -m pip install -r requirements.txt
+
+# only pip install if changes to requirements
+# startup is too slow otherwise!
+# https://stackoverflow.com/questions/17797740/check-if-specific-file-in-git-repository-has-changed
+if ! git diff --exit-code --quiet requirements.txt; then
+  echo "Updating dependings, sorry this is sloooooooooow....."
+  python -m pip install -r requirements.txt
+fi
+
 echo "Finally starting!  You should hear the startup sound."
 python themes.py
